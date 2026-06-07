7bb60b521beb31740affc2027186c18b9b9a242a
jordan <jordan@jordans-MBP.lan>
2026-06-07 06:49:14 -0500

    fix Stripe API version

--- app/create-checkout/route.ts
+++ app/create-checkout/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2026-05-27.dahlia',
 })
 
 const PRICE_IDS = {
